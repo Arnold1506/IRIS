bd1bf0780d34990a7497854eb37cf51c5ea8452c
Arnold1506 <aniketm1506@gmail.com>
2022-01-08 05:53:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# IRIS_2022
+ Template for IRIS 2022
---
+++ js/main.js
@@ -0,0 +1,51 @@
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// } 
+
+
+
+
+$(function() {
+	$(".block").on("click", function() {
+		$(".info").toggleClass("active");
+	});
+});
+
+// $( document ).ready(function() {
+//   console.log( "ready!" );
+// });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector(".main-header").classList.add("hactive");
+  } else {
+    document.querySelector(".main-header").classList.remove("hactive");
+  }
+} 
